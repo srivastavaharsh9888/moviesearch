67f744a763d9d7db7edcdcf7510f7821927cb792
harsh <srivastavaharsh9888@gmail.com>
2019-03-30 16:03:04 +0530

    Final production

--- frontend/moviesearch/src/environments/environment.prod.ts
+++ frontend/moviesearch/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
   serviceBaseURL:"https://server.searchmovie.ml/",
-  imagePosBaseURL:"https://image.tmdb.org/t/p/w500/",
-  imageBackBaseURL:"https://image.tmdb.org/t/p/w1280/",
+  imagePosBaseURL:"http://image.tmdb.org/t/p/w342/",
+  imageBackBaseURL:"http://image.tmdb.org/t/p/w1280/",
 };
