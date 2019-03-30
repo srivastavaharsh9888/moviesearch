cd803f43842e83254ea63932371ba1a5d00c596c
harsh <srivastavaharsh9888@gmail.com>
2019-03-30 16:07:40 +0530

    Https redirect corrected

--- frontend/moviesearch/src/environments/environment.prod.ts
+++ frontend/moviesearch/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
   serviceBaseURL:"https://server.searchmovie.ml/",
-  imagePosBaseURL:"http://image.tmdb.org/t/p/w342/",
-  imageBackBaseURL:"http://image.tmdb.org/t/p/w1280/",
+  imagePosBaseURL:"https://image.tmdb.org/t/p/w342/",
+  imageBackBaseURL:"https://image.tmdb.org/t/p/w1280/",
 };
