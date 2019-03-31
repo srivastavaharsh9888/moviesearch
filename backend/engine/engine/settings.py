eba938af19a7f7bfd4509960511b260440ca436d
Harsh Srivatava <srivastavaharsh9888@gmail.com>
2019-03-31 10:26:30 +0530

    Update settings.py
    Settings for postgresql

--- backend/engine/engine/settings.py
+++ backend/engine/engine/settings.py
@@ -91,8 +91,12 @@ WSGI_APPLICATION = 'engine.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.environ['DBS_NAME'],
+        'USER': os.environ['DBS_USER'],
+        'PASSWORD': os.environ['DBS_PASS'],
+        'HOST': os.environ['DBS_HOST'],
+        'PORT': '5432',
     }
 }
 
